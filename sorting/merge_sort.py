01f89d19f1c6e31f7b797ab3d9326f62f3019db3
Lucas Magnum <lucasmagnum2@gmail.com>
2018-12-21 12:08:22 +0100

    Merge sort: Part 2

--- sorting/merge_sort.py
+++ sorting/merge_sort.py
@@ -11,20 +11,62 @@
 
 
 def sort(array):
-    sort_half(array, 0, len(array) - 1)
+    aux = array[:]
+    sort_half(array, aux, 0, len(array) - 1)
 
 
-def sort_half(array, start, end):
+def sort_half(array, aux, start, end):
     if start >= end:
         return
 
     middle = (start + end) // 2
 
-    sort_half(array, start, middle)
-    sort_half(array, middle + 1, end)
+    sort_half(array, aux, start, middle)
+    sort_half(array, aux, middle + 1, end)
 
-    merge(array, start, end)
+    merge(array, aux, start, end)
 
 
-def merge(array, start, end):
-    array[start: end + 1] = sorted(array[start: end + 1])
+def merge(array, aux, start, end):
+    left = start
+    left_end = (start + end) // 2
+
+    right = left_end + 1
+    right_end = end
+
+    for position in range(start, end + 1):
+        #  There's no left element to be processed
+        if left > left_end:
+            aux[position] = array[right]
+            right += 1
+
+        #  There's no right element to be processed
+        elif right > right_end:
+            aux[position] = array[left]
+            left += 1
+
+        #  Compare left with right
+        elif array[left] < array[right]:
+            aux[position] = array[left]
+            left += 1
+
+        else:
+            aux[position] = array[right]
+            right += 1
+
+    for k in range(start, end + 1):
+        array[k] = aux[k]
+
+
+
+import random
+array = [random.randint(0, 100) for i in range(1000000)]
+copy = array[:]
+import time
+
+start = time.time()
+sort(array)
+end = time.time()
+print(end - start)
+
+assert array == sorted(copy)
